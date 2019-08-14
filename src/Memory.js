dea9e45197ac8899186040e598b007156a0407bc
Krzysztof Gryc <hello@krzysztofg.pl>
2019-08-14 15:30:32 +0200

    add buttons to state

--- src/Memory.js
+++ src/Memory.js
@@ -3,7 +3,26 @@ import './css/App.css';
 import Button from './Button';
 
 class Memory extends Component {
-    state = {}
+    state = {
+        buttons: [
+            { id: 0, name: 'A', clicked: false, disabled: false },
+            { id: 1, name: 'A', clicked: false, disabled: false },
+            { id: 2, name: 'B', clicked: false, disabled: false },
+            { id: 3, name: 'B', clicked: false, disabled: false },
+            { id: 4, name: 'C', clicked: false, disabled: false },
+            { id: 5, name: 'C', clicked: false, disabled: false },
+            { id: 6, name: 'D', clicked: false, disabled: false },
+            { id: 7, name: 'D', clicked: false, disabled: false },
+            { id: 8, name: 'E', clicked: false, disabled: false },
+            { id: 9, name: 'E', clicked: false, disabled: false },
+            { id: 10, name: 'F', clicked: false, disabled: false },
+            { id: 11, name: 'F', clicked: false, disabled: false },
+            { id: 12, name: 'G', clicked: false, disabled: false },
+            { id: 13, name: 'G', clicked: false, disabled: false },
+            { id: 14, name: 'H', clicked: false, disabled: false },
+            { id: 15, name: 'H', clicked: false, disabled: false },
+        ],
+    }
     render() {
         return (
             <div className="memory">
