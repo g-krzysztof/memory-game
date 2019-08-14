a103aad29d7f8966b59bf7cc822df4c0bdee13b8
Krzysztof Gryc <hello@krzysztofg.pl>
2019-08-14 23:55:14 +0200

    add button class ans styles

--- src/Button.js
+++ src/Button.js
@@ -4,11 +4,12 @@ const Button = (props) => {
     const buttons = props.buttons;
     return (
         buttons.map(button => <button
-            className="memory__btn"
+            className={button.disabled === true ? "memory__btn memory__btn--disabled" : "memory__btn"}
             key={button.id}
             id={button.id}
             onClick={props.onClick}
-            disabled={button.disabled}>
+            disabled={button.disabled}
+        >
             {button.clicked ? button.name : null}
         </button>)
     );
