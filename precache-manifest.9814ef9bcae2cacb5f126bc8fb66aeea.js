5a141f87ea1432801bdaffc3f556e21dba01cd7f
Krzysztof Gryc <hello@krzysztofg.pl>
2019-08-15 12:53:41 +0200

    Updates

---
+++ precache-manifest.9814ef9bcae2cacb5f126bc8fb66aeea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7cad64360eeaeab22fb4151c484789",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "3c983e888766940fe8c1",
+    "url": "/memory-game/static/css/main.fddadd27.chunk.css"
+  },
+  {
+    "revision": "5802a9c3d9be2e83f4b8",
+    "url": "/memory-game/static/js/2.4bc63d8a.chunk.js"
+  },
+  {
+    "revision": "3c983e888766940fe8c1",
+    "url": "/memory-game/static/js/main.09ac17d1.chunk.js"
+  },
+  {
+    "revision": "e2b6b13ff45de7602106",
+    "url": "/memory-game/static/js/runtime~main.e1bae424.js"
+  }
+]);
